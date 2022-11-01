823ee53db370ce63c866946d8a87385120a626a2
Michael Gee <mgee@bayviewglen.ca>
2022-11-01 10:24:36 -0400

    Database and Storage

---
+++ Introduction to JavaScript/database.js
@@ -0,0 +1,29 @@
+team = {
+    id: 1,
+    city: 'Toronto',
+    nickname: 'Toronto Blue Jays',
+
+    wins: 6,
+    losses: 6,
+
+    games: [
+        {
+            opponent: 7,
+            ourscore: 5,
+            oppscore: 3,
+            home: true
+        },
+        {
+            opponent: 7,
+            ourscore: 5,
+            oppscore: 3,
+            home: true
+        },
+        {
+            opponent: 7,
+            ourscore: 5,
+            oppscore: 3,
+            home: true
+        }
+    ]
+}
