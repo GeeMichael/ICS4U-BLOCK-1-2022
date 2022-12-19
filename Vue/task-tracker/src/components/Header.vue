73b58392768046c99d88ca22adf085fad5da5576
Michael Gee <mgee@bayviewglen.ca>
2022-12-19 09:51:06 -0500

    Vue crash course

---
+++ Vue/task-tracker/src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button />
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+
+    export default {
+        name: 'Header',
+        props: {
+            title: {
+                type: String,
+                default: 'Hello World',
+            }
+        },
+        components: {
+            Button
+        }
+    }
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
