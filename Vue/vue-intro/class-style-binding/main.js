f8b9c2ac85b07f317b878d5ee9dba8eb7a28d537
Michael Gee <mgee@bayviewglen.ca>
2023-02-02 09:47:15 -0500

    Vue Class & Style Binding

--- Vue/vue-intro/class-style-binding/main.js
+++ Vue/vue-intro/class-style-binding/main.js
@@ -5,7 +5,7 @@ const app = Vue.createApp({
          product: 'Socks',
          brand: 'Vue Mastery',
          image: './assets/images/socks_blue.jpg',
-         inStock: false,
+         inStock: true,
          details: ['50% cotton', '30% wool', '20% polyester'],
          variants: [
             { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
